dd4a9b1f5986240fdf8c150efe28898a1fdfe4dd
Ha Dung <70428921+Dung24-6@users.noreply.github.com>
2022-11-09 21:20:50 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -13,6 +13,13 @@ const textConfig = {
     "Still my love",
   text12: "Okii lunn <3",
 };
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome){
+      $('#iframeAudio').remove()
+  }
+  else {
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  }
 
 $(document).ready(function () {
   // process bar
